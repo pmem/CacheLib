2b43bc8a2eb06ae3bd6235ff9b8102b79de24918
Sounak Gupta <sounak.gupta@intel.com>
2022-07-21 02:01:04 -0700

    added per tier pool class rolling average latency

--- cachelib/cachebench/cache/CacheStats.h
+++ cachelib/cachebench/cache/CacheStats.h
@@ -193,6 +193,12 @@ struct Stats {
         out << folly::sformat("tid{:2} pid{:2} cid{:4} {:8.2f}{} free: {:4.2f}%",
           tid, pid, cid, allocSize, allocSizeSuffix, stats.approxFreePercent) << std::endl;
       });
+
+      foreachAC([&](auto tid, auto pid, auto cid, auto stats){
+        auto [allocSizeSuffix, allocSize] = formatMemory(stats.allocSize);
+        out << folly::sformat("tid{:2} pid{:2} cid{:4} {:8.2f}{} latency: {:8.2f}%",
+          tid, pid, cid, allocSize, allocSizeSuffix, stats.allocLatencyNs.estimate()) << std::endl;
+      });
     }
 
     out << folly::sformat("Tier 0 Background Evicted items : {:,}",
